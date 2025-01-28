9f15fe03ebf8524a3427702fe5e46f4d3a07f501
Yuliia_Sheremeta <sheremeyulya1996@gmail.com>
2025-01-28 23:37:29 +0100

    corrected errors

--- src/main.js
+++ src/main.js
@@ -11,48 +11,51 @@ let page = 1;
 let searchedQuery = '';
 let totalPages = 0;
 
+const lightbox = new SimpleLightbox('.gallery-card a', {
+            captionDelay: 250,
+            captionsData: 'alt',
+});
+        
 const onFormSubmit =  async event => {
         event.preventDefault();      
         searchedQuery = event.currentTarget.elements.search.value.trim();
    
     if (searchedQuery === '') {
         showAlert('Enter text to search!');
+
         return;
     }
-        page = 1;
-        loader.classList.add('active');
-        galleryEl.innerHTML = '';
-        btnLoadMore.classList.add('is-hidden');
+    page = 1;
+    loader.classList.add('active');
+    btnLoadMore.classList.add('is-hidden');
     
     try {
         const { data } = await fetchImages(searchedQuery, page);
 
+        
        if (data.hits.length === 0) {
-            showAlert('Sorry, there are no images matching your search query. Please try again!');
-            
-            formSearch.reset();
+           
+           showAlert('Sorry, there are no images matching your search query. Please try again!');
+           galleryEl.innerHTML = '';
+           formSearch.reset();
+           
             return;
         }
-       
-        totalPages = data.totalHits;
         
+        totalPages = Math.ceil(data.totalHits / 15);
+
         const imagesCardsTemplate = data.hits.map(el => createGalleryImgTemplate(el)).join('');
         galleryEl.innerHTML = imagesCardsTemplate;
- 
-        const lightbox = new SimpleLightbox('.gallery-card a', {
-            captionDelay: 250,
-            captionsData: 'alt',
-        });
 
-        lightbox.refresh();
-       
-        if (totalPages > 1) {
-            btnLoadMore.classList.remove('is-hidden')
+        if (data.totalHits > 15) {
+             btnLoadMore.classList.remove('is-hidden');
         };
-        
+     
+        lightbox.refresh();
+
    } catch (err) {
        console.log(err);
-       
+       showAlert('Something went wrong. Please try again later.');
     } finally {
         loader.classList.remove('active');
    }
@@ -60,43 +63,40 @@ const onFormSubmit =  async event => {
 
 formSearch.addEventListener('submit', onFormSubmit);
 
-const onLoadMoreBtnClick = async () => {
-    if (page < totalPages) {
+const onLoadMoreBtnClick = async event => {    
+    try {
         page++;
-        loader.classList.add('active');
-    } else {
-        return;
-    }
         
- try {
         const { data } = await fetchImages(searchedQuery, page);
+
+        if (data.hits.length === 0 || page >= totalPages) { 
+            btnLoadMore.classList.add('is-hidden');
+            showAlert("We're sorry, but you've reached the end of search results.");
+           
+            return;
+        }
+        
         const galleryTemplate = data.hits.map(el => createGalleryImgTemplate(el)).join('');
         galleryEl.insertAdjacentHTML('beforeend', galleryTemplate);
-
-     const lightbox = new SimpleLightbox('.gallery-card a', {
-            captionDelay: 250,
-            captionsData: 'alt',
-        });
-     lightbox.refresh();
      
-     const imgCard = galleryEl.querySelector('.gallery-card');
+        lightbox.refresh();
+        
+        const imgCard = galleryEl.querySelector('.gallery-card');
+        
      if (imgCard) { 
          const imgCardHeigth = imgCard.getBoundingClientRect().height;
-         window.scrollBy({
-             top: imgCardHeigth * 2,
+            
+            window.scrollBy({
+            top: imgCardHeigth * 2,
             behavior: 'smooth' 
          });
      };
 
-        if (page >= totalPages) { 
-            btnLoadMore.classList.add('is-hidden');
-            showAlert( "We're sorry, but you've reached the end of search results.");
-            btnLoadMore.removeEventListener('click', onLoadMoreBtnClick);
-           
-        };
 
 } catch (err) {
-    console.log(err);
+        console.log(err);
+        showAlert('Something went wrong. Please try again later.');
+
  } finally {
      loader.classList.remove('active');
 }
